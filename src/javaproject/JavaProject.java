374b489b87237a271124faa20bd69651aa5701b1
sohaila-ashraf-albdAllah <suhayla.alkwady@gmail.com>
2024-01-04 23:42:13 +0200

    Initial commit

---
+++ src/javaproject/JavaProject.java
@@ -0,0 +1,14 @@
+package javaproject;
+
+/**
+ *
+ * @author Sohaila Ashraf
+ */
+public class JavaProject 
+{
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}
